365eef276ea2af3b676fe85b69f5fa35e2c4cbb4
Fernando Costa <ferndcosta353@gmail.com>
2023-01-11 15:57:51 -0300

    added function to generate random password

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+
+
 let passwordLenght = 12;
 
 function getRandomCharacter() {
+    
     let randomCharacter = Math.floor(Math.random() * characters.length);
+
     return characters[randomCharacter];
+}
+
+function randomPasswordGenerator() {
+    
+    let randomPassword = "";
+
+    for (let i = 0; i < passwordLenght; i++) {
+        randomPassword += getRandomCharacter()
+    }
+
+    return randomPassword
 }
